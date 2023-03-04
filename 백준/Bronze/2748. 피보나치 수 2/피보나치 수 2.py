46309197ccf1d5aedf251737ef727d10ea7734e0
Sohyeon Jeon <shyeon02060@gmail.com>
2023-03-04 14:50:52 +0900

    [Bronze I] Title: 피보나치 수 2, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/2748. 피보나치 수 2/피보나치 수 2.py
+++ 백준/Bronze/2748. 피보나치 수 2/피보나치 수 2.py
@@ -3,10 +3,9 @@ input = sys.stdin.readline
 
 n = int(input())
 
-arr = [i for i in range(n+1)]
-arr[1] = 1
+arr = [i for i in range(n + 1)]
 
-for i in range(2, n+1) :
-    arr[i] = arr[i-1] + arr[i-2]
-    
+for i in range(2, n + 1):
+    arr[i] = arr[i - 1] + arr[i - 2]
+
 print(arr[-1])
