0e73fcda8574a7c626ac0ba1921367d6d4f361af
Sohyeon Jeon <shyeon02060@gmail.com>
2023-05-12 02:08:24 +0900

    [unrated] Title: 호텔 대실, Time: 1.42 ms, Memory: 10.8 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/155651. 호텔 대실/호텔 대실.py
@@ -0,0 +1,25 @@
+'''
+if 입장하는 손님의 입장 시간이 앞선 객실의 손님의 퇴장 시간보다 이후라면:
+기존 객실을 할당한다.
+else:
+새로운 객실을 할당한다.
+
+1. book_time을 분단위로 바꿔준다.
+'''
+import heapq
+
+def solution(book_time):
+    
+    book_time_minute = []
+    for s, e in book_time:
+        book_time_minute.append([int(s[:2]) * 60 + int(s[3:5]), int(e[:2]) * 60 + int(e[3:5])+10])
+    
+    book_time_minute.sort(key = lambda x:x[0])
+    
+    answer = []
+    for s, e in book_time_minute:
+        if answer and answer[0] <= s:
+            heapq.heappop(answer)
+        heapq.heappush(answer, e)
+            
+    return len(answer)
