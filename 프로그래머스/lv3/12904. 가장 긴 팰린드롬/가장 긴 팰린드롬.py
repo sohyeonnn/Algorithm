c8cd1d11664ce0fe67a4b53bb92253757d9ed97e
Sohyeon Jeon <shyeon02060@gmail.com>
2023-05-12 16:49:42 +0900

    [level 3] Title: 가장 긴 팰린드롬, Time: 3429.55 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv3/12904. 가장 긴 팰린드롬/가장 긴 팰린드롬.py
+++ 프로그래머스/lv3/12904. 가장 긴 팰린드롬/가장 긴 팰린드롬.py
@@ -2,8 +2,8 @@ def solution(s):
     answer = 0
     
     for i in range(len(s)):
-        for j in range(len(s), i, -1):
-            if s[i:j] == s[j:i:-1]:
+        for j in range(1, len(s)+1):
+            if s[i:j] == s[j:i-1]:
                 answer = max(answer, len(s[i:j]))
-                
-    return answer + 1
+            
+    return answer
