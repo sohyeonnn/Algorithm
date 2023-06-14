25d7869b1d5d458d4c9bc39de09e0029082a7ae7
Sohyeon Jeon <shyeon02060@gmail.com>
2023-06-14 14:05:05 +0900

    [unrated] Title: 옹알이 (2), Time: 0.11 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/unrated/133499. 옹알이 （2）/옹알이 （2）.py
+++ 프로그래머스/unrated/133499. 옹알이 （2）/옹알이 （2）.py
@@ -1,13 +1,15 @@
+#연속발음 예외케이스로 제외하기, replace로 가능한 발음 변환하기
 def solution(babbling):
     answer = 0
     can = ['aya', 'ye', 'woo', 'ma']
     
     for bab in babbling: 
         for c in can:
-            if c * 2 not in bab:  # 연속으로 나오지 않으면 공백(' ')으로 대체
+            if c * 2 not in bab:  #연속으로 나오지 않으면 공백(' ')으로 대체
                 bab = bab.replace(c, ' ')
+                print(bab)
                 
-        if bab.isspace():  # 공백으로만 이루어져 있으면 answer+1
-            answer += 1
+        if bab.isspace():  #문자열이 공백인지 체크하여
+            answer += 1     #공백으로만 이루어져 있으면 answer+1
             
     return answer
